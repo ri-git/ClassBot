065149a8cdbd012bb1ae0c82f701f639c5dd9bc7
ri-git <77895269+ri-git@users.noreply.github.com>
2021-03-06 17:37:32 +0530

    Update chromedriver.py

--- seleniumwork/chromedriver.py
+++ seleniumwork/chromedriver.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -14,6 +16,8 @@ locationChromeDriver ='../drivers/chromedriver'
 
 entryno = input("enter your entry number: ")
 pwd = input("enter your moodle password: ")
+mclass= input("enter monday's class: ")
+stime= input("enter start time: ")
 
 drive = webdriver.Chrome(executable_path=locationChromeDriver)
 drive.get(baseUrl)
@@ -43,8 +47,33 @@ cap = drive.find_element_by_name('valuepkg3')
 cap.send_keys(ans)
 cap.send_keys(Keys.RETURN)
 drive.find_element_by_link_text('3').click()
-drive.find_element_by_xpath('//*[@id="course-info-container-10887"]/div/div[2]/h4/a').click()
-drive.find_element_by_xpath('//*[@id="module-56927"]/div/div/div[2]/div/a/span').click()
+
+day1 = []  # monday routine
+day2 = []  # tuesday routine
+day3 = []  # wednesday routine
+day4 = []  # thursday routine
+day5 = []  # friday routine
+
+startclass = ["9:30",  ]  # starting time of classes in 24-hours format
+endclass = ["11:00",  ]  # ending time of classes in 24-hours format
+noc = 1
+if date.today().weekday() == 5:
+    print("MY BOT is checking MONDAY Timetable")
+    i = 0
+    while i <= 1:
+        print("Looking for class...")
+        i=2
+        for x in range(0, noc):
+            if time.strftime('%H:%M') == stime:
+                if mclass == "COL100":
+                    drive.find_element_by_xpath('//*[@id="course-info-container-10887"]/div/div[2]/h4/a').click()
+                    drive.find_element_by_xpath('//*[@id="module-56927"]/div/div/div[2]/div/a/span').click()
+                print("MY BOT : Its time for the class")
+                #classjoin(day1, x, endclass)
+                continue
+#drive.find_element_by_xpath('//*[@id="course-info-container-10887"]/div/div[2]/h4/a').click()
+#drive.find_element_by_xpath('//*[@id="module-56927"]/div/div/div[2]/div/a/span').click()
+
 
 
 
