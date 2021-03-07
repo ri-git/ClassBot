ba0c98e2333b3f41d832b0189ec88c07b1773fd1
Nishant Agarwal <nisagarwal0987@gmail.com>
2021-03-07 18:44:13 +0530

    ell101 has problem opening

--- DiscordBot/rev.py
+++ DiscordBot/rev.py
@@ -1,5 +1,5 @@
 from datetime import date
-
+import schedule
 #import self as self
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 import re
-
 
 
 import os
@@ -23,8 +22,10 @@ TOKEN = os.getenv('TOKEN')
 GUILD = os.getenv('GUILD')
 baseUrl = 'https://moodle.iitd.ac.in/login/index.php'
 locationChromeDriver ='C:\Program Files (x86)\chromedriver.exe'
-drive = webdriver.Chrome(executable_path=locationChromeDriver)
+# drive = webdriver.Chrome(executable_path=locationChromeDriver)
 client = discord.Client()
+
+# drive = webdriver.Chrome(executable_path=locationChromeDriver)
 
 @client.event
 async def on_ready():
@@ -59,18 +60,20 @@ async def on_message(message):
         entryno = input("enter your entry number: ")
         pwd = input("enter your moodle password: ")
         
-        def opMoodle()
-            drive.get(baseUrl)
 
-            search = drive.find_element_by_name('username')
-            search.send_keys(entryno)
-            spwd= drive.find_element_by_id('password')
-            spwd.send_keys(pwd)
+    def opMoodle():
+        drive = webdriver.Chrome(executable_path=locationChromeDriver)
+        drive.get(baseUrl)
+        
+        search = drive.find_element_by_name('username')
+        search.send_keys(entryno)
+        spwd= drive.find_element_by_id('password')
+        spwd.send_keys(pwd)
 
-            text = drive.find_element_by_id('login').text
-            temp = re.findall(r'\d+', text)
-            res= list(map(int, temp))
-            if 'first' in text:
+        text = drive.find_element_by_id('login').text
+        temp = re.findall(r'\d+', text)
+        res= list(map(int, temp))
+        if 'first' in text:
             ans = res[0]
 
             if 'second' in text:
@@ -106,7 +109,7 @@ async def on_message(message):
             temp = re.findall(r'\d+', text)
             res= list(map(int, temp))
             if 'first' in text:
-            ans = res[0]
+                ans = res[0]
 
             if 'second' in text:
                 ans = res[1]
@@ -129,23 +132,59 @@ async def on_message(message):
             page3.click()        
             drive.find_element_by_xpath('//*[@id="course-info-container-10887"]/div/div[2]/h4/a').click()
             drive.find_element_by_xpath('//*[@id="module-56927"]/div/div/div[2]/div/a/span').click()
-            drive.find_element_by_xpath('/html/body/div[1]/ui-view/div[1]/div[2]/ui-view/div/div[2]/div[2]/div/md-content/live-streaming-lectures/md-card/md-list/div[1]/div/div[2]/button/span').click()
-            time.sleep(7200)  #duration of a class?(seconds) 
-            driver.close()
+            drive.find_element_by_xpath('/html/body/d1iv[1]/ui-view/div[1]/div[2]/ui-view/div/div[2]/div[2]/div/md-content/live-streaming-lectures/md-card/md-list/div[1]/div/div[2]/button/span').click()
+            time.sleep(10)  #duration of a class?(seconds) 
+            drive.close()
 
     def opELL101():
-        opMoodle()
-        drive.execute_script("scrollBy(0,-1000);")
-        el = WebDriverWait(drive, 10).until( 
-                EC.presence_of_element_located((By.XPATH, '//*[@id="course-info-container-11053"]/div/div[2]/h4/a')) 
-            )
-        el.click()
-        drive.find_element_by_xpath('//*[@id="module-56737"]/div/div/div[2]/div/a/span').click()
-        drive.find_element_by_xpath('/html/body/div[1]/ui-view/div[1]/div[2]/ui-view/div/div[2]/div[2]/div/md-content/live-streaming-lectures/md-card/md-list/div[1]/div/div[2]/button/span').click()
-        time.sleep(7200)    #duration of a class?(seconds)
-        driver.close()
+        #opMoodle()
+            drive = webdriver.Chrome(executable_path=locationChromeDriver)
+            drive.get(baseUrl)
 
-    def opMTL101B():
+            search = drive.find_element_by_name('username')
+            search.send_keys(entryno)
+            spwd= drive.find_element_by_id('password')
+            spwd.send_keys(pwd)
+
+            text = drive.find_element_by_id('login').text
+            temp = re.findall(r'\d+', text)
+            res= list(map(int, temp))
+            if 'first' in text:
+                ans = res[0]
+
+            if 'second' in text:
+                ans = res[1]
+
+            if 'add' in text:
+                ans = res[0] + res[1]
+
+            if 'subtract' in text:
+                ans = res[0] - res[1]
+
+            cap = drive.find_element_by_name('valuepkg3')
+            cap.send_keys(ans)
+            cap.send_keys(Keys.RETURN)
+
+            # get element  after explicitly waiting for 10 seconds 
+            #page3 = WebDriverWait(drive, 10).until( 
+                #EC.presence_of_element_located((By.LINK_TEXT, "3")) 
+            #)
+            page3= drive.find_element_by_link_text('3')
+            page3.click()
+            drive.execute_script("scrollBy(0,-1000);")
+            el = WebDriverWait(drive, 10).until( 
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="course-info-container-11053"]/div/div[2]/h4/a')) 
+                )
+            el.click()
+            drive.find_element_by_xpath('//*[@id="module-56737"]/div/div/div[2]/div/a/span').click()
+            drive.find_element_by_xpath('/html/body/div[1]/ui-view/div[1]/div[2]/ui-view/div/div[2]/div[2]/div/md-content/live-streaming-lectures/md-card/md-list/div[1]/div/div[2]/button/span').click()
+            time.sleep(10)    #duration of a class?(seconds)
+            drive.close()
+
+
+    def opMTL101B():        
+        drive = webdriver.Chrome(executable_path=locationChromeDriver)
+        # drive.get(baseUrl)
         opMoodle()
         drive.execute_script("scrollBy(0,-1500);")
         mt = WebDriverWait(drive, 10).until( 
@@ -155,17 +194,18 @@ async def on_message(message):
         drive.find_element_by_xpath('//*[@id="module-59372"]/div/div/div[2]/div/a/span').click()
         drive.find_element_by_xpath('/html/body/div[1]/ui-view/div[1]/div[2]/ui-view/div/div[2]/div[2]/div/md-content/live-streaming-lectures/md-card/md-list/div[1]/div/div[2]/button/span').click()
         time.sleep(7200)    #duration of a class?(seconds)
-        driver.close()
+        #driver.close()
+        drive.close()
 
 
-    schedule.every().monday.at("09:28").do(opCOL100)
-    schedule.every().tuesday.at("09:58").do(opELL101)
-    schedule.every().tuesday.at("03:58").do(opMTL101B)
-    schedule.every().wednesday.at("09:58").do(opELL101)
-    schedule.every().wednesday.at("03:58").do(opMTL101B)
-    schedule.every().thursday.at("09:28").do(opCOL100)
-    schedule.every().friday.at("09:58").do(opELL101)
-    schedule.every().friday.at("03:58").do(opMTL101B)
+    # # schedule.every().sunday.at("18:42").do(opCOL100)
+    # schedule.every().tuesday.at("18:43").do(opCOL100)
+    # # schedule.every().tuesday.at("03:58").do(opMTL101B)
+    # # schedule.every().wednesday.at("09:58").do(opELL101)
+    # # schedule.every().wednesday.at("03:58").do(opMTL101B)
+    # # schedule.every().thursday.at("09:28").do(opCOL100)
+    # # schedule.every().friday.at("09:58").do(opELL101)
+    # # schedule.every().friday.at("03:58").do(opMTL101B)
 
     while True: 
   
