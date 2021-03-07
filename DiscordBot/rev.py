276cae794e830505c96385b6ff27eda0f9c0d3ef
Kartik-57 <77958514+Kartik-57@users.noreply.github.com>
2021-03-07 21:01:15 +0530

    Update rev.py

--- DiscordBot/rev.py
+++ DiscordBot/rev.py
@@ -242,6 +242,102 @@ async def on_message(message):
             drive.find_element_by_xpath('//*[ @ id = "label_3_22"] / span').click()
             drive.find_element_by_link_text('3').click()
 
+    mon = []
+    monstart = []
+    mondur = []
+    ncmon = (input("No. of classes on Monday: "))
+    ncmon = int(ncmon)
+    for i in range(ncmon):
+        print("For class ",i+1)
+        mon.append(input("Enter class name: "))
+        monstart.append(input("Enter class start time: "))  #hour:min 
+        mondur.append(input("Enter class duration in Seconds: "))
+        if mon[i] == "COL100":
+            schedule.every().monday.at(monstart[i]).do(opCOL100)
+        elif mon[i] == "ELL101":
+            schedule.every().monday.at(monstart[i]).do(opCOL100)
+        elif mon[i] == "MTL101":
+            schedule.every().monday.at(monstart[i]).do(opCOL100)
+        else:
+            print("Invalid Class Name")
+
+    tue = []
+    tuestart = []
+    tuedur = []
+    nctue = (input("No. of classes on Tuesday: "))
+    nctue = int(nctue)
+    for i in range(nctue):
+        print("For class ",i+1)
+        tue.append(input("Enter class name: "))
+        tuestart.append(input("Enter class start time: "))  #hour:min 
+        tuedur.append(input("Enter class duration in Seconds: "))
+        if tue[i] == "COL100":
+            schedule.every().tuesday.at(tuestart[i]).do(opCOL100)
+        elif tue[i] == "ELL101":
+            schedule.every().tuesday.at(tuestart[i]).do(opCOL100)
+        elif tue[i] == "MTL101":
+            schedule.every().tuesday.at(tuestart[i]).do(opCOL100)
+        else:
+            print("Invalid Class Name")
+
+    wed = []
+    wedstart = []
+    weddur = []
+    ncwed = (input("No. of classes on Wednesday: "))
+    ncwed = int(ncwed)
+    for i in range(ncwed):
+        print("For class ",i+1)
+        wed.append(input("Enter class name: "))
+        wedstart.append(input("Enter class start time: "))  #hour:min 
+        weddur.append(input("Enter class duration in Seconds: "))
+        if wed[i] == "COL100":
+            schedule.every().wednesday.at(wedstart[i]).do(opCOL100)
+        elif wed[i] == "ELL101":
+            schedule.every().wednesday.at(wedstart[i]).do(opCOL100)
+        elif wed[i] == "MTL101":
+            schedule.every().wednesday.at(wedstart[i]).do(opCOL100)
+        else:
+            print("Invalid Class Name")
+
+    thu = []
+    thustart = []
+    thudur = []
+    ncthu = (input("No. of classes on Thursday: "))
+    ncthu = int(ncthu)
+    for i in range(ncthu):
+        print("For class ",i+1)
+        thu.append(input("Enter class name: "))
+        thustart.append(input("Enter class start time: "))  #hour:min 
+        thudur.append(input("Enter class duration in Seconds: "))
+        if thu[i] == "COL100":
+            schedule.every().thursday.at(thustart[i]).do(opCOL100)
+        elif thu[i] == "ELL101":
+            schedule.every().thursday.at(thustart[i]).do(opCOL100)
+        elif thu[i] == "MTL101":
+            schedule.every().thursday.at(thustart[i]).do(opCOL100)
+        else:
+            print("Invalid Class Name")
+
+    fri = []
+    fristart = []
+    fridur = []
+    ncfri = (input("No. of classes on Friday: "))
+    ncfri = int(ncfri)
+    for i in range(ncfri):
+        print("For class ",i+1)
+        fri.append(input("Enter class name: "))
+        fristart.append(input("Enter class start time (Hr(24):Min): "))  #hour:min 
+        fridur.append(input("Enter class duration in Seconds: "))
+        if fri[i] == "COL100":
+            schedule.every().friday.at(fristart[i]).do(opCOL100)
+        elif fri[i] == "ELL101":
+            schedule.every().friday.at(fristart[i]).do(opCOL100)
+        elif fri[i] == "MTL101":
+            schedule.every().friday.at(fristart[i]).do(opCOL100)
+        else:
+            print("Invalid Class Name")
+
+
 
     # # schedule.every().sunday.at("18:42").do(opCOL100)
     # schedule.every().tuesday.at("18:43").do(opCOL100)
